f8b0110eec19f38915134f1ced79d88f6c6e7050
Aleksey Elchin <Elchin>
2025-02-17 15:52:44 +0900

    42 Объекты

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var a = 1 + 2;
+var b = a + 3;
+var c = {
+    apple: a,
+    banana: b
+};
+var d = c.apple * 4;
+var million = 1000000;
+console.log(typeof million);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+  apple: a,
+  banana: b,
+};
+let d = c.apple * 4;
+
+let million = 1000_000;
+
+console.log(typeof million);
